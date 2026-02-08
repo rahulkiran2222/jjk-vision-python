88b1ae935d31afb8ea114f7574f73175d1253814
rk2222 <rahulkiran2222@gmail.com>
2026-02-08 23:28:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 import cv2
 import numpy as np
-import mediapipe as mp
 
-# RESILIENT IMPORT LOGIC
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
+# DIRECT IMPORTS to bypass the "solutions" AttributeError
+import mediapipe as mp
+from mediapipe.python.solutions import hands as mp_hands
+from mediapipe.python.solutions import drawing_utils as mp_draw
+
+# Initialize Hands
 hands = mp_hands.Hands(
     static_image_mode=False,
     max_num_hands=2,
@@ -14,8 +16,8 @@ hands = mp_hands.Hands(
     min_tracking_confidence=0.7
 )
 
-class JJKTransformer(VideoTransformerBase):
-    def transform(self, frame):
+class JJKProcessor(VideoProcessorBase):
+    def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
         img = cv2.flip(img, 1)
         h, w, _ = img.shape
@@ -28,12 +30,11 @@ class JJKTransformer(VideoTransformerBase):
         color = (255, 255, 255)
 
         if results.multi_hand_landmarks:
-            hand_states = []
+            states = []
             for res in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+                mp_draw.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
                 
-                # Finger State Logic
-                # Index Tip (8) < Index Pip (6)
+                # Logic: Index(8), Middle(12), Ring(16), Pinky(20), Thumb(4)
                 s = {
                     'index': res.landmark[8].y < res.landmark[6].y,
                     'middle': res.landmark[12].y < res.landmark[10].y,
@@ -41,39 +42,40 @@ class JJKTransformer(VideoTransformerBase):
                     'pinky': res.landmark[20].y < res.landmark[18].y,
                     'thumb': res.landmark[4].y < res.landmark[2].y
                 }
-                hand_states.append(s)
+                states.append(s)
 
-            # --- MASTER GESTURE ENGINE ---
-            if len(hand_states) == 2:
-                # SHRINE: Double Palm (Both hands show index and pinky up)
-                if hand_states[0]['index'] and hand_states[0]['pinky'] and \
-                   hand_states[1]['index'] and hand_states[1]['pinky']:
-                    active_tech = "MALEVOLENT SHRINE"
+            # GESTURE ENGINE
+            if len(states) == 2:
+                # MALEVOLENT SHRINE: Both Palms Open
+                if states[0]['index'] and states[1]['index'] and states[0]['pinky'] and states[1]['pinky']:
+                    active_tech = "SHRINE"
                     color = (0, 0, 255)
-            elif len(hand_states) == 1:
-                h1 = hand_states[0]
-                # PURPLE: Open Palm (All 4 main fingers up)
+            elif len(states) == 1:
+                h1 = states[0]
+                # HOLLOW PURPLE: Open Palm
                 if h1['index'] and h1['middle'] and h1['ring'] and h1['pinky']:
-                    active_tech = "HOLLOW PURPLE"
-                    color = (255, 0, 150)
-                # RED: Rock-on (Thumb, Index, Pinky up, Middle/Ring down)
+                    active_tech = "PURPLE"
+                    color = (255, 0, 180)
+                # RED: Rock-on (Thumb, Index, Pinky UP)
                 elif h1['thumb'] and h1['index'] and h1['pinky'] and not h1['middle']:
-                    active_tech = "REVERSAL: RED"
+                    active_tech = "RED"
                     color = (0, 0, 255)
-                # VOID: Peace Sign (Index, Middle up, Ring down)
+                # VOID: Peace Sign (Index, Middle UP)
                 elif h1['index'] and h1['middle'] and not h1['ring']:
-                    active_tech = "UNLIMITED VOID"
-                    color = (255, 100, 0)
+                    active_tech = "VOID"
+                    color = (255, 120, 0)
 
-        # Apply Visuals
-        if active_tech != "NONE":
-            cv2.putText(img, active_tech, (50, 100), cv2.FONT_HERSHEY_TRIPLEX, 2, color, 5)
-            if active_tech == "UNLIMITED VOID":
-                img = cv2.bitwise_not(img)
-            else:
-                cv2.circle(img, (w//2, h//2), 160, color, -1)
+        # Draw Visuals
+        if active_tech == "VOID":
+            img = cv2.bitwise_not(img)
+            cv2.putText(img, "UNLIMITED VOID", (50, 100), cv2.FONT_HERSHEY_TRIPLEX, 2, (255,255,255), 5)
+        elif active_tech != "NONE":
+            cv2.circle(img, (w//2, h//2), 150, color, -1)
+            cv2.putText(img, active_tech, (50, 100), cv2.FONT_HERSHEY_TRIPLEX, 2, (255,255,255), 5)
 
-        return img
+        return frame.from_ndarray(img, format="bgr24")
 
-st.title("Jujutsu High Vision System")
-webrtc_streamer(key="jjk", video_transformer_factory=JJKTransformer)
+st.title("Jujutsu High Vision System 🧿")
+st.markdown("Peace (Void) | Rock-on (Red) | Palm (Purple) | 2 Palms (Shrine)")
+
+webrtc_streamer(key="jjk", video_processor_factory=JJKProcessor)
